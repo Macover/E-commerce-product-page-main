d264bba794373925b0005222f76f2f8477c6af01
Macover <macover1507@hotmail.com>
2022-02-26 15:36:19 -0600

    input function added

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 "use strict";
 
-let cartButton = document.querySelector("#cartButton");
+const cartButton = document.querySelector("#cartButton");
+const inputNumber = document.querySelector("#inputNumber");
+const minusButton = document.querySelector("#minusButton");
+const addButton = document.querySelector("#addButton");
 
-console.log(cartButton)
+let inputNumberValue = 0;
 
+const validateInputNumber = e =>{
+    if(isNaN(e.key)) inputNumber.value = 0;
+    else inputNumber.value = e.key;
+}
 
+inputNumber.addEventListener("keyup",e=>validateInputNumber(e));
 
+addButton.addEventListener("click",()=>{    
+    if(inputNumber.value > 8) inputNumber.value = 9;
+    else inputNumber.value++;
+});
+minusButton.addEventListener("click",()=>{    
+    if(inputNumber.value < 1) inputNumber.value = 0;
+    else inputNumber.value--;
+});
