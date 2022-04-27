03e41a182191010f3a40905d636bbe23bc2311c9
Isaí <macover@iMac-de-Isai.local>
2022-04-26 19:45:52 -0500

    Desktop mode: Notification cart when click in add to cart button

--- script-desktop.js
+++ script-desktop.js
@@ -8,16 +8,16 @@ const modal = document.querySelector("#modalImg");
 const primaryImageD = document.querySelector("#primaryImageD");
 const modalCloseButton = document.querySelector("#modalCloseButton");
 
-modalCloseButton.addEventListener("click",()=>{
+modalCloseButton.addEventListener("click", () => {
     console.log("first")
-    if(modal.classList.contains("onOpenFlex")){
+    if (modal.classList.contains("onOpenFlex")) {
         modal.classList.remove("onOpenFlex");
         modal.classList.add("onClose");
     }
 });
 
-primaryImageD.addEventListener("click",()=>{
-    if(modal.classList.contains("onClose")){
+primaryImageD.addEventListener("click", () => {
+    if (modal.classList.contains("onClose")) {
         modal.classList.remove("onClose");
         modal.classList.add("onOpenFlex");
     }
@@ -25,19 +25,19 @@ primaryImageD.addEventListener("click",()=>{
 
 let inputNumberValue = 0;
 
-const validateInputNumber = e =>{
-    if(isNaN(e.key)) inputNumber.value = 0;
+const validateInputNumber = e => {
+    if (isNaN(e.key)) inputNumber.value = 0;
     else inputNumber.value = e.key;
 }
 
-inputNumber.addEventListener("keyup",e=>validateInputNumber(e));
+inputNumber.addEventListener("keyup", e => validateInputNumber(e));
 
-addButton.addEventListener("click",()=>{    
-    if(inputNumber.value > 8) inputNumber.value = 9;
+addButton.addEventListener("click", () => {
+    if (inputNumber.value > 8) inputNumber.value = 9;
     else inputNumber.value++;
 });
-minusButton.addEventListener("click",()=>{    
-    if(inputNumber.value < 1) inputNumber.value = 0;
+minusButton.addEventListener("click", () => {
+    if (inputNumber.value < 1) inputNumber.value = 0;
     else inputNumber.value--;
 });
 
@@ -45,20 +45,41 @@ minusButton.addEventListener("click",()=>{
 
 const deskCardCart = document.getElementById("deskCardCart");
 
-cartButton.addEventListener("click",()=>{
+cartButton.addEventListener("click", () => {
 
-    
-    if(cartButton.style.backgroundColor != "var(--nBlack)"){
+
+    if (cartButton.style.backgroundColor != "var(--nBlack)") {
         cartButton.style.backgroundColor = "var(--nBlack)"
-    }else{
+    } else {
         cartButton.style.backgroundColor = "var(--nDrkGrayishBlue)"
     }
 
-    if(deskCardCart.classList.contains("onClose")){
+    if (deskCardCart.classList.contains("onClose")) {
         deskCardCart.classList.remove("onClose");
         deskCardCart.classList.toggle("onOpenFlex");
-    }else{
+    } else {
         deskCardCart.classList.remove("onOpenFlex");
         deskCardCart.classList.toggle("onClose");
     }
 })
+
+//add an item to the cart functionality.
+
+const addToCartButton = document.getElementById('addToCartButton');
+
+addToCartButton.addEventListener("click", () => {
+
+    const notificationCart = document.getElementById('notificationCart');
+    const notificationCartNumber = document.getElementById('notificationCartNumber');
+    
+    notificationCartNumber.textContent = inputNumber.value;
+
+    if (inputNumber.value == 0) {
+        notificationCart.classList.replace("onOpenFlex", "onClose");
+    } else {        
+        notificationCart.classList.replace("onClose", "onOpenFlex");
+    }
+})
+
+
+
